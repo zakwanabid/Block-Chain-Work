1b61ba013964ca959516f5ae8f9342ac530d972a
zakwan abid <56184420+zakwanabid@users.noreply.github.com>
2020-12-10 21:42:32 +0500

    Add files via upload

---
+++ Node JS Task/splice.js
@@ -0,0 +1,13 @@
+console.log("=====CV Using Slice======");
+
+var cv =["Zakwan","Abid",20,"BSCS","PAF-KIET","Reading Books","IoT , Block Chain",1234567,"zakwan@gmail.com"];
+
+console.log("Name: " +cv.slice(0,1))
+console.log("Last Name: " +cv.slice(1,2))
+console.log("Age: " +cv.slice(2,3))
+console.log("Education: " +cv.slice(3,4))
+console.log("University: "+cv.slice(4,5))
+console.log("Hobby: " +cv.slice(5,6))
+console.log("Certtifiction: " +cv.slice(6,7))
+console.log("Phone No: " +cv.slice(7,8))
+console.log("Gmail: " +cv.slice(8,9))
