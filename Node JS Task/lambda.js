171269b1839a2413a019bd5d4e615eee96909e42
zakwan abid <56184420+zakwanabid@users.noreply.github.com>
2020-12-10 21:39:15 +0500

    Add files via upload

---
+++ Node JS Task/lambda.js
@@ -0,0 +1,69 @@
+
+//CV using Lambda Function
+
+console.log("=========== Resume ==============","\n"); 
+
+     var name = () => { 
+     console.log("Name: Zakwan Abid"); 
+    }
+
+    var f_name = () => { 
+    console.log("Last Name: Abid Hussain"); 
+    }
+
+    var mobile = () => { 
+     console.log("Ph No: 123456789"); 
+    } 
+
+    var email = () => { 
+        console.log("Email: abc@gmail.com"); 
+    }
+
+     var qualification = () => { 
+         console.log("Qualification: Matric, Intermediate, BSCS 3rd Semester ");
+    }
+
+    var experience = () => { 
+              console.log("Experience: 2.5 Years in Teaching"); 
+    }
+
+    var certification = () => {
+        console.log("Certification: CCNA, Kespersky lab, CISSP");
+    }
+name() 
+f_name() 
+mobile() 
+email() 
+qualification() 
+experience()
+certification()
+
+
+//Calculator Using Lamda Function 
+
+console.log("============= CALCULATOR ============","\n");
+
+var add = (a,b) => sum = a + b
+{ 
+    console.log("The Addition is :",add(6,9));
+ } 
+ 
+ var sub = (c,d) => sub = c - d 
+ { 
+     console.log("The Subtraction is :",sub(32,8)); 
+}
+
+var mult = (e,f) => mult = e * f
+{ 
+    console.log("The Modulus is :",mult(30,6)); 
+} 
+        
+var div = (g,h) => div = g / h 
+{
+    console.log("The Division is :",div(10,5)); 
+}
+
+
+
+
+
