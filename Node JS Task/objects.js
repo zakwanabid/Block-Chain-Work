0051e31126baeafe701440983292cecaa377f28d
zakwan abid <56184420+zakwanabid@users.noreply.github.com>
2020-12-10 21:41:35 +0500

    Add files via upload

---
+++ Node JS Task/objects.js
@@ -0,0 +1,111 @@
+// ******** CV through Objects *********
+
+// console.log("===== RESUME =======");
+
+// var cv = {
+//     fname : "Zakwan Abid",
+//     Lname : "Abid Hussain",
+//     Age : "20 Years",
+//     Education : "Matric, Intermediate, BS in Computer Science",
+//     Hobby : "Reading Books",
+//     Certification : "In my Certification includes CCNA, Python, Kespersky Lab",
+//     Gmail : "ABC@gmail.com"
+// }
+
+// console.log("Name: " +cv.fname);
+// console.log("Last Name: "+cv.Lname);
+// console.log("Age: " + cv.Age);
+// console.log("Education: " + cv.Education);
+// console.log("Hobby: "+ cv.Hobby);
+// console.log("Certification: " + cv.Certification );
+// console.log("Gmail: " + cv.Gmail);
+
+
+
+// ****** Marksheet using Objects ********
+
+// It was not run in Terminal/CMD. It will run on Browser
+
+// console.log(" ***** Marksheet ******");
+
+// var marksheet = []
+
+// marksheet[0] = prompt("Enter Data Structure Marks: ")
+// marksheet[1] = prompt("Enter OOP Marks: ")
+// marksheet[2] = prompt("Enter Calculus Marks: ")
+// marksheet[3] = prompt("Enter Chemestry Marks: ")
+// marksheet[4] = prompt("Enter Physics Marks: ")
+// marksheet[5] = prompt("Enter English Marks: ")
+
+// let Avgmarks = +marksheet[0]+ + +marksheet[1]+ + +marksheet[2]+ + +marksheet[3]+ + +marksheet[4]+ + +marksheet[5] 
+
+// let avg = parseInt(Avgmarks/600 * 100);
+
+// console.log("Total Marks: 600")
+// console.log(" Marks Obtain: " + Avgmarks)
+
+// var sheet = {
+//         Data_Structure:   marksheet[0],
+//         OOP:        marksheet[1],
+//         Calculus:   marksheet[2],
+//         Chemestry:  marksheet[3],
+//         Physics:   marksheet[4],
+//         English:    marksheet[5]     
+// }
+// console.log(" Data Structure Marks: " + sheet.Data_Structure)
+// console.log(" OP Marks: " + sheet.OOP)
+// console.log(" Calculus Marks: " + sheet.Calculus)
+// console.log(" Chemestry Marks: " + sheet.Chemestry)
+// console.log(" Physics Marks: " + sheet.Physics)
+// console.log(" English Marks: " + sheet.English)
+
+// if (avg > 86 || avg === 100)
+// {
+//   console.log("Grade : A+");      
+//   } 
+// else if (avg > 71 || avg ===85) 
+// {
+//         console.log("Grade : A"); 
+//         } 
+// else if (avg >= 61 || avg ===70) 
+//  {
+//         console.log("Grade : B"); 
+// } 
+// else if (avg >= 51 || avg ===60) 
+// {
+//         console.log("Grade : C"); 
+// } 
+// else if ( avg < 50)
+//  {
+//         console.log("FAIL"); 
+//  }
+
+
+
+// ***** Careem APP Using Objects ********
+
+// console.log("***** Careem Application *****")
+
+// var careem = {
+//     Client : ("Zakwan Abid"),
+//     pickup_location : ("ABC Block 5, Street 1"),
+//     Destination : ("SSUET University Karachi"),
+//     Pno : (123456778),
+//     car : ("Bussiness CAR"),
+    
+//     Driver : ("Herry"),
+//     time : ("Your Driver is on the way!"),
+//     Amount : ("Your Amount is 1200. Please pay via CASH"),
+//     feedback : ("Thank you for USING careem.")
+// }
+
+// console.log("Client: " +careem.Client)
+// console.log("Pickup Location: " +careem.pickup_location)
+// console.log("Where you want to go: " +careem.Destination)
+// console.log("Phone Number: " +careem.Pno)
+// console.log("Car Name: " +careem.car)
+// console.log("Your Driver is: " +careem.Driver)
+// console.log("When will driver reach: " +careem.time)
+// console.log("Pay Amount: " + careem.Amount)
+// console.log("Feedback: " + careem.feedback)
+
