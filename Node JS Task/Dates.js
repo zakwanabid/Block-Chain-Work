d594696c10b613d35c653edc9cafdf778f783814
zakwan abid <56184420+zakwanabid@users.noreply.github.com>
2020-12-10 21:38:33 +0500

    Add files via upload

---
+++ Node JS Task/Dates.js
@@ -0,0 +1,21 @@
+console.log(" ******** Date/Time *******", "\n");
+
+var now = new Date();
+
+console.log(now);
+
+console.log("\n");
+
+var it = new Date();
+
+console.log("Year is: "+it.getFullYear());
+
+console.log("Date is: "+ it.getDate());
+
+console.log("Hour is: "+it.getHours());
+
+console.log("Month is: "+it.getMonth());
+
+console.log("Time is: "+it.getTime());
+
+console.log("Day is: "+it.getDay());
